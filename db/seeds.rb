be5cbe7e6002f567e894436d3e5cb2a958316d35
Prima Aulia Gusta <primAulia@gmail.com>
2020-09-11 13:33:52 +0800

    prep for reboot

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+puts "PREP the DB"
+
+Artist.destroy_all
+Genre.destroy_all
+# TODO: destroy albums that the artist owned
+
+puts "START SEED"
+
+
+def seed_artists
+  api_key = "api_key=key5wiIECTI6NOYbO"
+  base_url = "https://api.airtable.com/v0/appOnpFap3mruBMNc/artists?#{api_key}"
+  
+  offset = ""
+  while offset
+    url = offset == "" ? base_url : "#{base_url}&offset=#{offset}"
+    json = JSON.parse(open(url).read)
+    json["records"].each do |data|
+      Artist.create!(name: data["fields"]["name"])
+    end
+    offset = json["offset"]
+  end
+  puts "DONE CREATING artists"
+end
+
+def seed_genres
+  api_key = "api_key=key5wiIECTI6NOYbO"
+  base_url = "https://api.airtable.com/v0/appOnpFap3mruBMNc/genres?#{api_key}"
+  
+  offset = ""
+  while offset
+    url = offset == "" ? base_url : "#{base_url}&offset=#{offset}"
+    json = JSON.parse(open(url).read)
+    json["records"].each do |data|
+      Genre.create!(name: data["fields"]["name"])
+    end
+    offset = json["offset"]
+  end
+  puts "DONE CREATING genres"
+end
+
+def seed_album
+  # TODO
+end
+
+seed_artists
+seed_genres
+seed_albums
+
